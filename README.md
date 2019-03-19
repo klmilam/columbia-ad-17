c88f60ac473d34c618f6d46a7ff8bf20a9d8c3b4
Kimberly Milam <kmilam@kmilam-macbookpro3.roam.corp.google.com>
2019-03-19 12:08:40 -0400

    get filenames from list_blobs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # columbia-ad-17
+
+$ mkvirtualenv <environment name>
+$ pip install -r requirements.txt
+$ gcloud auth application-default login
--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
 import tensorflow as tf
 import nibabel as nib
 import numpy as np
@@ -16,8 +18,10 @@ def _int64_feature(value):
 def _float_feature(value):
   return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
+
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
 
 def serialize_example(image, label):
   #TODO: should data be ravelled?
@@ -29,21 +33,32 @@ def serialize_example(image, label):
   return example_proto.SerializeToString()
 
 
-def create_tfrecord(filenames, bucket_name, labels, tfrecord_filename):
+def create_tfrecord(bucket_name, prefix, labels, tfrecord_filename):
   """Load each image and convert to add to TFRecord
   TFRecord is added to local directory
   """
   local_filename = "example.nii"
+  credentials = service_account.Credentials.from_service_account_file(INPUT_API_KEY)
+  client = storage.Client(credentials=credentials, project="columbia-dl-storage")
+  bucket = client.get_bucket(bucket_name)
+  blobs = bucket.list_blobs(prefix=prefix)
+  counter = 0
   with tf.python_io.TFRecordWriter(tfrecord_filename) as writer:
-    for filename in filenames:
-      print(filename)
-      read_file_from_GCS(bucket_name, filename, INPUT_API_KEY, "columbia-dl-storage",
-          local_filename)
-      image = nib.load(local_filename)
-      image_data = image.get_fdata()
-      label = find_label(labels, filename)
-      example = serialize_example(image_data, label)
-      writer.write(example)
+    for blob in blobs:
+      if blob.name != "data/":
+        print(blob.name)
+        b = bucket.blob(blob.name)
+        b.download_to_filename(local_filename)
+        image = nib.load(local_filename)
+        image_data = image.get_fdata()
+        label = find_label(labels, blob.name)
+        if label != "": #ignore examples where label not found
+          example = serialize_example(image_data, label)
+          writer.write(example)
+        counter = counter + 1
+      if counter > 4:
+        break
+
 
 def read_file_from_GCS(bucket_name, filename, api_key_path, project, output_filename=""):
   credentials = service_account.Credentials.from_service_account_file(api_key_path)
@@ -53,8 +68,7 @@ def read_file_from_GCS(bucket_name, filename, api_key_path, project, output_file
   if output_filename == "":
     return blob.download_as_string()
   else:
-    blob.download_to_filename(output_filename) 
-
+    blob.download_to_filename(output_filename)
 
 def get_labels_array(bucket_name, filename):
   output = read_file_from_GCS(bucket_name, filename, INPUT_API_KEY, "columbia-dl-storage")
@@ -62,20 +76,24 @@ def get_labels_array(bucket_name, filename):
   trimmed_output = csv_output[["Subject","T1.SERIESID","Group"]]
   return trimmed_output
 
+
 def find_label(labels, filename):
   subject = "_".join(filename.split("/")[1].split("_")[0:3])
   seriesID = filename.split("/")[1].split("_")[3:4][0][1:]
-  label = labels[labels["Subject"] == subject][labels["T1.SERIESID"] == int(seriesID)]
-  return label["Group"].item()
+  seriesID = seriesID.split("-")[0]
+  try:
+    label = labels[labels["Subject"] == subject][labels["T1.SERIESID"] == int(seriesID)]
+    return label["Group"].item()
+  except:
+    print("Series "+string(seriesID) + ", Subject" + string(subject) + " not found")
+    return ""
 
 def run(filename, bucket_name, output_tf_filename):
   """Creates TFRecords and adds them to local directory
   """
   #filenames = os.listdir(input_dir)
-  #read_file_from_GCS(bucket_name, filename, INPUT_API_KEY, "columbia-dl-storage",
-  #    "example.nii")
   labels = get_labels_array(bucket_name, "ADNI_t1_list_with_fsstatus_20190111.csv")
-  create_tfrecord([filename], bucket_name, labels, output_tf_filename)
+  create_tfrecord(bucket_name, "data", labels, output_tf_filename)
 
 
 def main():
