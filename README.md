6216f28c44b3b4334cba30be72a6a68ab7184ce5
Kimberly Milam <kmilam@kmilam-macbookpro3.roam.corp.google.com>
2019-04-30 01:33:24 -0400

    Add necessary files for Dataflow preprocessing

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 $ mkvirtualenv <environment name>
 $ pip install -r requirements.txt
 $ gcloud auth application-default login
+
+export GOOGLE_APPLICATION_CREDENTIALS='path/to/columbia-dl-storage-bucket key'
+
+python ./preprocessor/test.py --setup_file ./setup.py
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+  'tensorflow>=1.13.1',
+  #'tensorflow-gpu==2.0.0-alpha0',
+  'dltk',
+  'nibabel',
+  'numpy>=1.14.2',
+  'pandas>=0.23.4',
+  'six'
+]
+
+setup(
+    name='preprocessing',
+    version='0.1',
+    author='Kim Milam',
+    install_requires=REQUIRED_PACKAGES,
+    packages=['preprocessor','trainer']
+)
