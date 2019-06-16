ba36a688e1f35bdc0d8689ec977ee63291c3e760
Kimberly Milam <kmilam@google.com>
2019-06-16 16:24:20 -0400

    Initial commit for new task.py

---
+++ trainer/trainer/task.py
@@ -0,0 +1,102 @@
+"""Trainer for 3D CNN MRI model."""
+
+import sys
+import argparse
+import functools
+
+import tensorflow as tf
+import tensorflow_transform as tft
+
+SEED = 123
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--verbosity',
+        choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+        default='INFO'
+    )
+    parser.add_argument(
+        '--train-batch-size',
+        help='Batch size for each training step',
+        type=int,
+        default=120
+    )
+    parser.add_argument(
+        '--eval-batch-size',
+        help='Batch size for evaluation steps',
+        type=int,
+        default=1000
+    )
+    parser.add_argument(
+        '--eval-start-secs',
+        help='How long to wait before starting first evaluation',
+        default=20,
+        type=int
+    )
+    parser.add_argument(
+        '--eval-steps',
+        help="""Number of steps to run evaluation for at each checkpoint',
+        Set to None to evaluate on the whole evaluation data
+        """,
+        default=None,
+        type=int
+    )
+    parser.add_argument(
+        '--num-epochs',
+        help="""Maximum number of training data epochs.
+        If both --train-size and --num-epochs are specified, --train-steps will
+        be: (train-size/train-batch-size) * num-epochs.""",
+        default=None,
+        type=int
+    )
+    parser.add_argument(
+        '--job-dir',
+        help='GCS location to write checkpoints and export models',
+        required=True
+    )
+    parser.add_argument(
+        '--input-dir',
+        help='GCS or local directory to TFRecord and metadata files.',
+        required=True
+    )
+    parser.add_argument(
+        '--train-steps',
+        help="""Steps to run the training job for.
+        If --num-epochs and --train-size are specified, then --train-steps will
+        be: (train-size/train-batch-size) * num-epochs""",
+        default=10000,
+        type=int
+    )
+    parser.add_argument(
+        '--train-size',
+        help='Size of training set (instance count)',
+        type=int,
+        default=None
+    )
+    parser.add_argument(
+        '--learning-rate',
+        help='Learning rate',
+        default=0.021388123321319803,
+        type=float
+    )
+    return parser.parse_args(argv)
+
+def train_and_evaluate(flags):
+    """Runs model training and evaluation using TF Estimator API"""
+    #Get TF transform metadata generated during preprocessing
+    tf_transform_output = tft.TFTransformOutput(flags.input_dir)
+
+def main():
+    #Parse command-line arguments
+    flags = parse_arguments(sys.argv[1:])
+
+    #Set python level verbosity
+    tf.logging.set_verbosity(flags.verbosity)
+    
+    #Run model training and evaluate
+    train_and_evaluate(flags)
+
+if __name__ == '__main__':
+    main()
