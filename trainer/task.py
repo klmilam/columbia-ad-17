1e6f1cfcfb53f5d7ede822a79db57ced4fb9cf9e
Kimberly Milam <kmilam@google.com>
2019-09-15 18:25:27 -0400

    add CLI args and TPU flags

--- trainer/task.py
+++ trainer/task.py
@@ -20,36 +20,32 @@ SEED = 123
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(sys.path[0]))))
 
 tf.flags.DEFINE_string(
-    "tpu", default=None,
-    help="The Cloud TPU to use for training. This should be either the name "
-    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
-    "url.")
+    'tpu',
+    default=None,
+    help="""The Cloud TPU to use for training. This should be either the name
+         used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470
+         url.""")
 tf.flags.DEFINE_string(
-    "tpu_zone", default=None,
-    help="[Optional] GCE zone where the Cloud TPU is located in. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+    'tpu_zone',
+    default=None,
+    help="""[Optional] GCE zone where the Cloud TPU is located in. If not
+         specified, we will attempt to automatically detect the GCE project from
+         metadata.""")
 tf.flags.DEFINE_string(
-    "gcp_project", default=None,
-    help="[Optional] Project name for the Cloud TPU-enabled project. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+    'gcp_project',
+    default=None,
+    help="""[Optional] Project name for the Cloud TPU-enabled project. If not 
+         specified, we will attempt to automatically detect the GCE project from 
+         metadata.""")
 
 # Model specific parameters
-tf.flags.DEFINE_string("data_dir", "",
-                         "Path to directory containing the MNIST dataset")
-tf.flags.DEFINE_string("model_dir", None, "Estimator model_dir")
-tf.flags.DEFINE_integer("train_steps", 1000, "Total number of training steps.")
-tf.flags.DEFINE_integer("eval_steps", 10,
-                        "Total number of evaluation steps. If `0`, evaluation "
-                        "after training is skipped.")
-tf.flags.DEFINE_float("learning_rate", 0.0000005, "Learning rate.")
 
-tf.flags.DEFINE_bool("use_tpu", True, "Use TPUs rather than plain CPUs")
-tf.flags.DEFINE_bool("enable_predict", True, "Do some predictions at the end")
-tf.flags.DEFINE_integer("iterations", 30,
-                        "Number of iterations per TPU training loop.")
-tf.flags.DEFINE_integer("num_shards", 8, "Number of shards (TPU chips).")
+
+tf.flags.DEFINE_bool('use_tpu', True, 'Use TPUs rather than plain CPUs')
+tf.flags.DEFINE_bool('enable_predict', True, 'Do some predictions at the end')
+tf.flags.DEFINE_integer('iterations', 30,
+                        'Number of iterations per TPU training loop.')
+tf.flags.DEFINE_integer('num_shards', 8, 'Number of shards (TPU chips).')
 
 FLAGS = tf.flags.FLAGS
 
@@ -66,9 +62,40 @@ def parse_arguments(argv):
         '--fixed-weights',
         default=[1,1,1,1,1,1]
     )
+    parser.add_argument(
+        '--data-dir',
+        type='str',
+        default='gs://internal-klm-tpu/mri/128_128_16/20190915162022/'
+    )
+    parser.add_argument(
+        '--model-dir',
+        type='str',
+        default='gs://internal-klm-tpu/mri/model/'+ datetime.now().strftime('%Y%m%d%H%M%S'),
+        help="""Location to write checkpoints and summaries to.
+             Must be a GCS URI when using Cloud TPU."""
+    )
+    parser.add_argument(
+        '--train-steps',
+        type=int,
+        default=1000,
+        help='Total number of training steps.'
+    )
+    parser.add_argument(
+        '--eval-steps',
+        type=int,
+        default=10,
+        help='Total number of eval steps. If `0`, evaluation after training is skipped.'
+    )
+    parser.add_argument(
+        '--learning-rate',
+        type=float,
+        default='0.0000005',
+        help='learning rate'
+    )
     return parser.parse_args(argv)
 
-def train_and_evaluate(flags):
+
+def train_and_evaluate(params):
     """Runs model training and evaluation using TF Estimator API"""
     tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
         FLAGS.tpu,
@@ -78,14 +105,14 @@ def train_and_evaluate(flags):
 
 
 def main():
-    #Parse command-line arguments
-    flags = parse_arguments(sys.argv[1:])
+    # Parse command-line arguments
+    params = parse_arguments(sys.argv[1:])
 
-    #Set python level verbosity
     tf.logging.set_verbosity(flags.verbosity)
     
     #Run model training and evaluate
-    train_and_evaluate(flags)
+    train_and_evaluate(params)
+
 
 if __name__ == "__main__":
     tf.logging.set_verbosity(tf.logging.DEBUG)
