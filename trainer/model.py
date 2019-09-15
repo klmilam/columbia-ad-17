6701053fbe0447af7c6918d8772e6f96107f1090
Kimberly Milam <kmilam@google.com>
2019-09-15 18:55:31 -0400

    start updating model code

--- trainer/model.py
+++ trainer/model.py
@@ -1,139 +1,137 @@
 """TensorFlow model definition."""
 
-import tensorflow as tf
+def metric_fn(labels, logits):
+
+    def precision(labels, logits, class_id):
+        return tf.metrics.precision_at_k(
+            labels=labels,
+            predictions=logits,
+            k=1,
+            class_id=class_id)
 
 
-TF_NUMERIC_TYPES = [
-    tf.float16,
-    tf.float32,
-    tf.float64,
-    tf.int8,
-    tf.int16,
-    tf.int32,
-    tf.int64,
-]
+    def recall(labels, logits, class_id):
+        return tf.metrics.recall_at_k(
+            labels=labels,
+            predictions=logits,
+            k=1,
+            class_id=class_id)
 
 
-def get_feature_columns(tf_transform_output, exclude_columns=[]):
-    """Returns list of feature columns for a TensorFlow estimator.
-    Args:
-        tf_transform_output: tensorflow_transform.TFTransformOutput.
-        exclude_columns: `tf_transform_ooutput` column names to be excluded
-            from feature columns.
-
-    Returns:
-        List of TensorFlow feature columns.
-    """
-    feature_columns = []
-    feature_spec = tf_transform_output.transformed_feature_spec()
-
-    for col in exclude_columns:
-        _ = feature_spec.pop(col, None)
-
-    for k, v in feature_spec.items():
-        if v.dtype in TF_NUMERIC_TYPES:
-            feature_columns.append(tf.feature_column.numeric_column(
-                k, dtype=v.dtype))
-        elif v.dtype == tf.string:
-            vocab_file = tf_transform_output.vocabulary_file_by_name(
-                vocab_filename=k)
-            feature_column = \
-                tf.feature_column.categorical_column_with_vocabulary_file(
-                    k,
-                    vocab_file)
-            feature_columns.append(tf.feature_column.indicator_column(
-                feature_column))
-    return feature_columns
-
-
-def _cnn_model_fn(features, labels, mode, params):
-    """3D CNN model to classify Alzheimer's."""
-    #Create model
-
-    #Compute predictions
-    input_layer = tf.reshape(features, [-1, 256, 256, 256, 1])
-    conv1 = tf.layers.conv3d(
-        inputs=input_layer,
-        filters=64,
-        kernel_size=[7,7,7],
-        strides=[2, 2, 2],
-        padding='same',
-        activation=tf.nn.relu
+    predictions = tf.argmax(logits, axis=1)
+    accuracy = tf.metrics.accuracy(
+        labels=labels, predictions=predictions)
+    # calculate accuracy for each class, then takes the mean of that
+    mean_per_class_accuracy = tf.metrics.mean_per_class_accuracy(
+        labels=labels,
+        predictions=predictions,
+        num_classes=6
     )
-    pool1 = tf.layers.max_pooling3d(
-        inputs=conv1, pool_size=[3, 3, 3], strides=2)
-    conv2a = tf.layers.conv3d(
-        inputs=pool1,
-        filters=256,
-        kernel_size=[1,1,1],
-        strides=2,
-        padding='same',
-        activation=tf.nn.relu
-    )
-    conv2b = tf.layers.conv3d(
-        inputs=conv2a,
-        filters=64,
-        kernel_size=[3,3,3],
-        strides=2,
-        padding='same',
-        activation=tf.nn.relu
-    )
-    conv2c = tf.layers.conv3d(
-        inputs=conv2b,
-        filters=64,
-        kernel_size=[1,1,1],
-        strides=2,
-        padding='same',
-        activation=tf.nn.relu
-    )
-    pool2 = tf.layers.max_pooling3d(
-        inputs=conv2c, pool_size=[2,2,2], strides=2)
-    pool4_flat = tf.layers.flatten(pool2)
-    dense1 = tf.layers.dense(
-        inputs=pool4_flat, units=32, activation=tf.nn.relu)
-    dense2 = tf.layers.dense(
-        inputs=dense1, units=64, activation=tf.nn.relu)
-    logits = tf.layers.dense(
-        inputs=dense2, units=6, activation=tf.nn.softmax,
-        kernel_initializer=tf.random_normal_initializer(stddev=.01))
-    one_hot_labels = tf.one_hot(labels, 6)
-    loss = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits, labels=labels))
+    labels = tf.cast(labels, tf.int64)
+    output = {
+        "accuracy": accuracy,
+        "mean_per_class_accuracy": mean_per_class_accuracy
+    }
+    for i in range(0, 6):
+        key = "precision_class_"+str(i)
+        output[key] = precision(labels, logits, i)
+    for i in range(0, 6):
+        key = "recall_class_"+str(i)
+        output[key] = recall(labels, logits, i)
+    return output
+
+def model_fn(features, labels, mode, params):
+    """Constructs 3D CNN model"""
+
+    image = features
+
+    if isinstance(image, dict):
+        image = features["image"]
+
+    with tf.contrib.tpu.bfloat16_scope():
+
+        x = tf.reshape(features, [-1, 128, 128, 16, 1])
+
+        conv1 = tf.layers.conv3d(
+            inputs=x,
+            filters=16,
+            kernel_size=[3,3,3],
+            activation=tf.nn.relu
+        )
+
+        conv2 = tf.layers.conv3d(
+            inputs=conv1,
+            filters=32,
+            kernel_size=[3,3,3],
+            activation=tf.nn.relu
+        )
+
+        pool1 = tf.layers.max_pooling3d(
+            inputs=conv2,
+            pool_size=[2, 2, 2],
+            strides=[2, 2, 2]
+        )
+
+        conv4 = tf.layers.conv3d(
+            inputs=pool1,
+            filters=64,
+            kernel_size=[3,3,3],
+            activation=tf.nn.relu
+        )
+
+        conv5 = tf.layers.conv3d(
+            inputs=conv4,
+            filters=128,
+            kernel_size=[3,3,3],
+            activation=tf.nn.relu
+        )
+
+        pool2 = tf.layers.max_pooling3d(
+            inputs=conv5,
+            pool_size=[2, 2, 2],
+            strides=[2, 2, 2]
+        )
+
+        pool2_flat = tf.layers.flatten(pool2)
+
+        dense2 = tf.layers.dense(
+            inputs=pool2_flat, units=512, activation=tf.nn.relu)
+
+        batch_norm = tf.layers.batch_normalization(dense2)
+
+        # regularization
+        dropout = tf.layers.dropout(batch_norm, rate=0.5)
+
+        logits = tf.layers.dense(
+            inputs=dropout, units=6)
+
+        logits = tf.cast(logits, tf.float32) # Casting necessary to use bfloat32
+        probabilities = tf.nn.softmax(logits)
+        class_ids = tf.argmax(logits, axis=1)
+
     if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = {}
-        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        # Compute the current epoch and associated learning rate from global_step.
-        global_step = tf.train.get_global_step()
-        batches_per_epoch = 7943/params['batch_size']
-        learning_rate = tf.train.exponential_decay(
-            learning_rate=0.01, global_step=global_step,
-            decay_steps=100, decay_rate=0.005)
-        optimizer = tf.train.GradientDescentOptimizer(
-            learning_rate=0.0005)
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            train_op = optimizer.minimize(loss,
-                global_step=global_step)
-        gs_t = tf.reshape(global_step, [1])
-        loss_t = tf.reshape(loss, [1])
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            loss=loss,
-            train_op=train_op)
-    return tf.estimatr.EstimatorSpec(
-        mode,
-        loss=loss)
-
-def build_estimator(run_config, flags, feature_columns):
-    """Returns TensorFlow estimator"""
-
-    estimator = tf.estimator.Estimator(
-        model_fn=_cnn_model_fn,
-        model_dir=flags.job_dir,
-        config=run_config,
-        params={
-            'batch_size': flags.train_batch_size
+        predictions = {
+            'class_ids': class_ids,
+            'probabilities': probabilities
         }
-    )
-    return estimator
+        return tf.contrib.tpu.TPUEstimatorSpec(
+            mode=mode,
+            predictions=predictions
+        )
+
+    labels_onehot = tf.one_hot(labels, 6)
+    counts = tf.reduce_sum(labels_onehot, axis=0)
+
+    if params.weight_type == "global_frequency":
+        counts = params.fixed_weights
+        class_weights = np.sum(counts)/counts
+        class_weights = tf.reshape(class_weights, [6,1])
+    elif params.weight_type == "batch_frequency":
+        class_weights = np.sum(counts)/counts
+        class_weights = tf.reshape(class_weights, [6,1])
+    else:
+        beta = .999
+        class_weights = (1.0 - beta)/(1.0 - tf.math.pow(beta, counts))
+        class_weights = tf.reshape(class_weights, [6,1])
+        class_weights = tf.reshape(
+           class_weights/tf.reduce_sum(class_weights), [6,1])
--- trainer/task.py
+++ trainer/task.py
@@ -4,15 +4,15 @@ import sys
 import os
 import argparse
 import functools
+from datetime import datetime
+import numpy as np
 
 import tensorflow as tf
 import tensorflow_transform as tft
 
 from absl import app as absl_app
 
-from trainer import input_util
-from trainer import metadata
-from trainer import model
+import model
 
 SEED = 123
 
@@ -54,8 +54,8 @@ def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--weight-type',
-        type='str',
-        default="",
+        type=str,
+        default='',
         help="""Pass 'fixed' to use fixed class weights (based on global frequency)"""
     )
     parser.add_argument(
@@ -64,12 +64,12 @@ def parse_arguments(argv):
     )
     parser.add_argument(
         '--data-dir',
-        type='str',
+        type=str,
         default='gs://internal-klm-tpu/mri/128_128_16/20190915162022/'
     )
     parser.add_argument(
         '--model-dir',
-        type='str',
+        type=str,
         default='gs://internal-klm-tpu/mri/model/'+ datetime.now().strftime('%Y%m%d%H%M%S'),
         help="""Location to write checkpoints and summaries to.
              Must be a GCS URI when using Cloud TPU."""
@@ -92,23 +92,66 @@ def parse_arguments(argv):
         default='0.0000005',
         help='learning rate'
     )
+    parser.add_argument(
+        '--train-batch-size',
+        type=int,
+        default=1024,
+        help='Batch size for training'
+    )
+    parser.add_argument(
+        '--eval-batch-size',
+        type=int,
+        default=1024,
+        help='Batch size for evaluation'
+    )
+    parser.add_argument(
+        '--predict-batch-size',
+        type=int,
+        default=1024,
+        help='Batch size for prediction'
+    )
     return parser.parse_args(argv)
 
 
 def train_and_evaluate(params):
     """Runs model training and evaluation using TF Estimator API"""
+
     tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
         FLAGS.tpu,
         zone=FLAGS.tpu_zone,
         project=FLAGS.gcp_project
     )
 
+    run_config = tf.contrib.tpu.RunConfig(
+        cluster=tpu_cluster_resolver,
+        model_dir=params.model_dir,
+        save_summary_steps=100,
+        session_config=tf.ConfigProto(
+            allow_soft_placement=True, log_device_placement=True),
+        save_checkpoints_steps=100,
+        tpu_config=tf.contrib.tpu.TPUConfig(
+            iterations_per_loop=40,
+            num_shards=FLAGS.num_shards,
+            per_host_input_for_training=tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
+        )
+    )
 
-def main():
+    estimator = tf.contrib.tpu.TPUEstimator(
+        model_fn=model.model_fn,
+        use_tpu=FLAGS.use_tpu,
+        train_batch_size=params.train_batch_size,
+        eval_batch_size=params.eval_batch_size,
+        predict_batch_size=params.predict_batch_size,
+        params={
+            "data_dir": params.data_dir,
+            "class_weights": params.class_weights,
+            "fixed_weights": np.asarray(params.fixed_weights)},
+        config=run_config)
+
+
+def main(argv):
     # Parse command-line arguments
-    params = parse_arguments(sys.argv[1:])
-
-    tf.logging.set_verbosity(flags.verbosity)
+    params = parse_arguments(argv[1:])
     
     #Run model training and evaluate
     train_and_evaluate(params)
