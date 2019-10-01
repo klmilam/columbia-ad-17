4516410fa5ad9e94a70d7f4f294fe0bd30e0b709
Kimberly Milam <kmilam@google.com>
2019-10-01 13:17:35 -0400

    move input fn to input_util.py

--- trainer/input_util.py
+++ trainer/input_util.py
@@ -1,61 +1,65 @@
 """Input functions."""
 
-import os
-import multiprocessing
+def input_fn(input_dir, mode, num_epochs=100, label_name=None,
+    feature_spec=None, params={}):
+    """Reads TFRecords and returns the features and labels for all datasets."""
 
-import tensorflow as tf
-import tensorflow_transform as tft
-
-
-
-def input_fn(input_dir, mode, batch_size=1, num_epochs=100,
-    label_name=None, feature_spec=None):
-    """Reads TFRecords and returns the features and labels"""
     def read_and_decode_fn(example):
-        """Parses Serialized Example."""
+        """Parses a Serialized Example."""
         features = tf.parse_single_example(example, feature_spec)
         image = features['image']
-        image = tf.reshape(image, [256, 256, 256])
+        image = tf.cast(image, tf.bfloat16) # TPUs don't support float64
+        image = tf.reshape(image, shape=(160, 160, 32))
         label = tf.cast(features[label_name], tf.int32)
         return image, label
 
-    if feature_spec is None:
-        tf_transform_output = tft.TFTransformOutput(
-            os.path.join(input_dir, 'transformed_metadata'))
-        feature_spec = tf_transform_output.transformed_feature_spec()
+    def random_flip(image, label) -> tf.Tensor:
+        """Randomly flips a training image for data augmentation purposes."""
+        image = tf.image.random_flip_up_down(image)
+        return image, label
+
+    def random_crop(image, label) -> tf.Tensor:
+        """Randomly crops a training image for data augmentation purposes."""
+        image = tf.image.random_crop(image, size=[128, 128, 16])
+        return image, label
+
+    def crop(image, label) -> tf.Tensor:
+        """Deterministically crops a testing or eval image.
+
+        Returns:
+            image: the middle 128 x 128 x 16 cube (a Tensor) of the input
+                image.
+            label: the un-transformed label
+        """
+        image = tf.slice(image, [16, 16, 8], [128, 128, 16])
+        return image, label
 
     prefix = str(mode).lower()
+    prefix = 'predict' if prefix == 'infer' else prefix
     suffix = '.tfrecord'
-    num_cpus = multiprocessing.cpu_count()
-    file_pattern = os.path.join(input_dir, 'data', prefix, prefix+'*'+suffix)
-    filenames = tf.matching_files(file_pattern)
-    dataset = tf.data.TFRecordDataset(filenames=filenames, buffer_size=None,
-                                      num_parallel_reads=num_cpus)
+    file_pattern = os.path.join(
+        input_dir, 'data', prefix, prefix + '*' + suffix)
+    tf.logging.info(prefix + ' data from ' + file_pattern)
+    filenames = tf.matching_files(file_pattern) # list of TFRecords files
+
+    dataset = tf.data.TFRecordDataset(filenames=filenames, buffer_size=None)
+    dataset = dataset.map(read_and_decode_fn, num_parallel_calls=100)
+
     if mode == tf.estimator.ModeKeys.TRAIN:
-        dataset = dataset.apply(
-            tf.data.experimental.shuffle_and_repeat(buffer_size=100)
-        )
-    dataset = dataset.apply(
-        tf.data.experimental.map_and_batch(
-            map_func=read_and_decode_fn,
-            batch_size=batch_size,
-            num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    )
-    iterator = dataset.make_one_shot_iterator()
-    features, labels = iterator.get_next()
+        # Apply data augmentation transformations to training dataset
+        dataset = dataset.map(random_crop, num_parallel_calls=100)
+        dataset = dataset.map(random_flip, num_parallel_calls=100)
+    else:
+        dataset = dataset.map(crop, num_parallel_calls=100)
+
+    dataset = dataset.batch(
+        params['batch_size'],
+        drop_remainder=True) # Must drop remainder when working with TPUs
+    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return features
-    return features, labels
+        # Not necessary to repeat or shuffle prediction dataset
+        return dataset
 
-
-def tfrecord_serving_input_fn(feature_spec, label_name=None):
-    """Creates ServingInputReceiver for TFRecord inputs"""
-    if label_name:
-        _ = feature_spec.pop(label_name)
-
-    serving_input_receiver = (
-        tf.estimator.export.build_parsing_serving_input_receiver_fn(
-            feature_spec)())
-
-    return tf.estimator.export.ServingInputReceiver(
-        serving_input_receiver.features, serving_input_receiver.receiver_tensors)
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(50)
+    return dataset
--- trainer/task.py
+++ trainer/task.py
@@ -13,6 +13,7 @@ import tensorflow_transform as tft
 from absl import app as absl_app
 
 import model
+import input_util
 
 SEED = 123
 
@@ -112,71 +113,6 @@ def parse_arguments(argv):
     return parser.parse_args(argv)
 
 
-def input_fn(input_dir, mode, num_epochs=100, label_name=None,
-    feature_spec=None, params={}):
-    """Reads TFRecords and returns the features and labels for all datasets."""
-
-    def read_and_decode_fn(example):
-        """Parses a Serialized Example."""
-        features = tf.parse_single_example(example, feature_spec)
-        image = features['image']
-        image = tf.cast(image, tf.bfloat16) # TPUs don't support float64
-        image = tf.reshape(image, shape=(160, 160, 32))
-        label = tf.cast(features[label_name], tf.int32)
-        return image, label
-
-    def random_flip(image, label) -> tf.Tensor:
-        """Randomly flips a training image for data augmentation purposes."""
-        image = tf.image.random_flip_up_down(image)
-        return image, label
-
-    def random_crop(image, label) -> tf.Tensor:
-        """Randomly crops a training image for data augmentation purposes."""
-        image = tf.image.random_crop(image, size=[128, 128, 16])
-        return image, label
-
-    def crop(image, label) -> tf.Tensor:
-        """Deterministically crops a testing or eval image.
-
-        Returns:
-            image: the middle 128 x 128 x 16 cube (a Tensor) of the input
-                image.
-            label: the un-transformed label
-        """
-        image = tf.slice(image, [16, 16, 8], [128, 128, 16])
-        return image, label
-
-    prefix = str(mode).lower()
-    prefix = 'predict' if prefix == 'infer' else prefix
-    suffix = '.tfrecord'
-    file_pattern = os.path.join(
-        input_dir, 'data', prefix, prefix + '*' + suffix)
-    tf.logging.info(prefix + ' data from ' + file_pattern)
-    filenames = tf.matching_files(file_pattern) # list of TFRecords files
-
-    dataset = tf.data.TFRecordDataset(filenames=filenames, buffer_size=None)
-    dataset = dataset.map(read_and_decode_fn, num_parallel_calls=100)
-
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        # Apply data augmentation transformations to training dataset
-        dataset = dataset.map(random_crop, num_parallel_calls=100)
-        dataset = dataset.map(random_flip, num_parallel_calls=100)
-    else:
-        dataset = dataset.map(crop, num_parallel_calls=100)
-
-    dataset = dataset.batch(
-        params['batch_size'],
-        drop_remainder=True) # Must drop remainder when working with TPUs
-    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        # Not necessary to repeat or shuffle prediction dataset
-        return dataset
-
-    dataset = dataset.repeat()
-    dataset = dataset.shuffle(50)
-    return dataset
-
-
 def train_and_evaluate(params):
     """Runs model training and evaluation using TF Estimator API"""
 
@@ -216,7 +152,7 @@ def train_and_evaluate(params):
     feature_spec = tf_transform_output.transformed_feature_spec()
 
     train_input_fn = functools.partial(
-        input_fn,
+        input_util.input_fn,
         input_dir,
         tf.estimator.ModeKeys.TRAIN,
         num_epochs=100,
@@ -224,7 +160,7 @@ def train_and_evaluate(params):
         feature_spec=feature_spec)
 
     eval_input_fn = functools.partial(
-        input_fn,
+        input_util.input_fn,
         input_dir,
         tf.estimator.ModeKeys.EVAL,
         num_epochs=1,
@@ -232,7 +168,7 @@ def train_and_evaluate(params):
         feature_spec=feature_spec)
 
     predict_input_fn = functools.partial(
-        input_fn,
+        input_util.input_fn,
         input_dir,
         tf.estimator.ModeKeys.PREDICT,
         num_epochs=1,
